701b7522d7a36b348028d85a2636969f6a7f423c
AK-Polash <polashk199@gmail.com>
2022-11-01 10:16:20 +0600

    project completed

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,36 @@
+let inputData = document.querySelector("#input");
+let buttton = document.querySelector(".button");
 let evenHeading = document.querySelector(".even");
 let oddHeading = document.querySelector(".odd");
-let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-let arrayTwo = [];
-let arrayThree = [];
+let myArray = [];
+let evenArr = [];
+let oddArr = [];
 
-myArray.map((item) => {
-  let result = item % 2;
-  if (result === 0) {
-    arrayTwo.push(item);
-    evenHeading.innerHTML = "Even numbers : ";
-    evenHeading.innerHTML += arrayTwo;
-  }
-  if (result !== 0) {
-    arrayThree.push(item);
-    oddHeading.innerHTML = "Odd numbers : ";
-    oddHeading.innerHTML += arrayThree;
-  }
+buttton.addEventListener("click", () => {
+  myArray.push(inputData.value);
+  inputData.value = "";
+  console.log(myArray);
+
+  myArray.map((item) => {
+    let result = item % 2;
+
+    if (result === 0) {
+      evenArr.push(item);
+      evenArr = evenArr.filter((evItem, evIndex) => {
+        return evenArr.indexOf(evItem) === evIndex;
+      });
+      evenHeading.innerHTML = "Even numbers : ";
+      evenHeading.innerHTML += evenArr;
+      console.log("evenArr: " + evenArr);
+    }
+    if (result !== 0) {
+      oddArr.push(item);
+      oddArr = oddArr.filter((odItem, odIndex) => {
+        return oddArr.indexOf(odItem) === odIndex;
+      });
+      oddHeading.innerHTML = "Odd numbers : ";
+      oddHeading.innerHTML += oddArr;
+      console.log("oddArr: " + oddArr);
+    }
+  });
 });
